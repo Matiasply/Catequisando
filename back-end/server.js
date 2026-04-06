cb560d09a67af8590b3a7bddbb7a0ef1b7002c0c
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-06 10:39:38 -0300

    Chamando rota de autenticação na api

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
