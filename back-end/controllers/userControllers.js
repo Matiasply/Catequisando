6283b83f1fc39fedc0305b6db3e3afb527f3a3e3
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-06 10:07:22 -0300

    Salva a senha criptografada

--- back-end/controllers/userControllers.js
+++ back-end/controllers/userControllers.js
@@ -1,10 +1,14 @@
 const userModel = require('../model/userModel');
+const bcrypt = require('bcrypt'); //Criptografia para as senhas dos usuários
 
 async function createUser(req, res) {
     const { name, password } = req.body;
 
+    // Criptografa a senha antes de armazenar no banco de dados
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     try {
-        const newUser = await userModel.createUser(name, password);
+        const newUser = await userModel.createUser(name, hashedPassword);
         res.status(201).json(newUser);
     } catch (error) {
         res.status(500).json({ error: 'Error creating user' });
