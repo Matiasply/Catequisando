0966ce6625d50aedbdfd9ddb6af7480008982486
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-10 10:50:47 -0300

    O nome certo é seção

--- back-end/controllers/sessionControllers.js
+++ back-end/controllers/sessionControllers.js
@@ -3,15 +3,15 @@ const sessionModel = require("../model/sessionModel")
 
 async function createSession(req, res) {
 
-    const nome_sessao = req.body.name;
-    console.log(nome_sessao)
+    const nome_secao = req.body.name;
+    console.log(nome_secao)
 
     try {
-        await sessionModel.createSession(nome_sessao);
-        res.status(201).json({message: `Session ${nome_sessao} created!`});
+        await sessionModel.createSession(nome_secao);
+        res.status(201).json({message: `Session ${nome_secao} created!`});
     } catch (error) {
         console.error("Error creating session: ", error);
-       return res.status(500).json({error: `Error creating session: ${nome_sessao}`});
+       return res.status(500).json({error: `Error creating session: ${nome_secao}`});
        
     }
 }
