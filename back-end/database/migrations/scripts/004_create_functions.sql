6a7fd936515a3a9bdf635cc6776325d0260f987a
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-13 10:50:32 -0300

    Função chamada para consulta de pegar tudo em uma seção

---
+++ back-end/database/migrations/scripts/004_create_functions.sql
@@ -0,0 +1,32 @@
+
+CREATE OR REPLACE FUNCTION get_secao_completa(p_sessao_id INT)
+RETURNS TABLE (
+  sessao_id INT,
+  sessao_nome TEXT,
+  modulo_id INT,
+  modulo_nome TEXT,
+  submodulo_id INT,
+  submodulo_nome TEXT,
+  aula_id INT,
+  nome TEXT
+)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT
+    s.id_secao,
+    s.nome_secao,
+    m.id_modulo,
+    m.nome_modulo,
+    sm.id_submodulo,
+    sm.nome_submodulo,
+    a.id_aula,
+    a.nome
+  FROM secoes s
+  LEFT JOIN modulos m ON m.id_secao = s.id_secao
+  LEFT JOIN submodulos sm ON sm.id_modulo = m.id_modulo
+  LEFT JOIN aula a ON a.id_submodulo = sm.id_submodulo
+  WHERE s.id_secao = p_sessao_id
+  ORDER BY m.ordem ASC, sm.ordem ASC, a.ordem ASC;
+END;
+$$ LANGUAGE plpgsql;
