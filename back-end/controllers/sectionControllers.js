0613c6572fe91aeeaed81e0985d2f036b3f815d9
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-23 10:33:36 -0300

    Função para  formatar a resposta

--- back-end/controllers/sectionControllers.js
+++ back-end/controllers/sectionControllers.js
@@ -17,13 +17,55 @@ async function createSection(req, res) {
     }
 }
 
+async function formatar_resposta(rows) {
+
+const resultado = {
+  id: rows[0].secao_id,
+  nome: rows[0].secao_nome,
+  conteudo: []
+};
+
+rows.forEach(row => {
+  let modulo = resultado.conteudo.find(m => m.id === row.modulo_id);
+
+  if (!modulo) {
+    modulo = {
+      id: row.modulo_id,
+      nome: row.modulo_nome,
+      submodulos: []
+    };
+    resultado.conteudo.push(modulo);
+  }
+
+  let submodulo = modulo.submodulos.find(s => s.id === row.submodulo_id);
+
+  if (!submodulo) {
+    submodulo = {
+      id: row.submodulo_id,
+      nome: row.submodulo_nome,
+      aulas: []
+    };
+    modulo.submodulos.push(submodulo);
+  }
+
+  if (row.aula_id) {
+      submodulo.aulas.push({
+      id: row.aula_id,
+      nome: row.nome
+    });
+  }
+});
+    return resultado;
+}
+
 async function getAllSection(req, res) {
 
     const id_secao = req.params.id;
 
     try {
         const result = await sectionModel.getAllSection(id_secao)
-        res.status(200).json({result})
+        const resultado = await formatar_resposta(result)
+        res.status(200).json({resultado})
     } catch (error) {
         console.error("Error getting section", error)
         res.status(500).json({error: 'Error getting section info'})
